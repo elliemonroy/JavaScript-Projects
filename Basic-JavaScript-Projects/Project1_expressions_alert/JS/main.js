55823f796fd16fb953fdeaf5dcec720e34207c6c
elliemonroy <122836246+elliemonroy@users.noreply.github.com>
2023-02-01 15:34:08 -0800

    This is my Basic_JavaScript Projects Assignment

---
+++ Basic-JavaScript-Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,34 @@
+window.alert("Hello, world!"); //This is a function that makes an alert window say "Hello, world!"
+
+document.write('Hello, world!');  //This is a function that writes 'hello, world' on my html document
+
+var E = " Ellie"; //Defining E as the variable and giving it a string value
+
+document.write(E); //Using the function to write my string value on my html document
+
+var b = "This is my string value"; //Defining b as the variable and giving it a string value
+
+window.alert(b); //Using the function to write my string value on my html document
+
+document.write(" \"I don\'t like green eggs and ham\" said sam."); //Here I have string that has quoation marks and an 
+                                                                  //apostrophe, so I'm using \ escaping the character
+                                                                  //so computer knows this means something and doesn't end the string
+
+var A = " apple" + "s";  //Defining A as the variable and concatenating the string "apple" and the string "s"
+
+document.write(A); //Using the function to write my strings "apples" on my html document
+
+var dog = "breed", dog1 = "American Pitbull Terrior", dog2 = " Cane Corso", dog3 = "Dobermann", dog4 = "Goldendoodle"; //Here I'm assigning    
+                                                                                                                      //multiple variables in
+                                                                                                                      //one statement
+
+document.write(dog2); //Using the function to write the string that I assigned to variable dog2 on my html document
+
+var Sent1 = " This is the beginning of the string" //Defining Sent1 as the varialbe and giving it a string value
+
+var Sent2 = " and this is the end of the string "  //Defining Sent2 as the varialbe and giving it a string value
+
+document.write(Sent1 + Sent2); //Using the concatenating symbol to put my two strings together
+
+document.write (4 + 4); //Using the function to write an expression on my html document
+
